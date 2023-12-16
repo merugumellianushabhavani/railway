0574fa2b6b4a43a2ebf5966052dd105c5992a578
merugumellianushabhavani <anushabhavani07@gmail.com>
2023-12-16 11:42:41 +0530

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from myapp.views import *
+app_name='myapp'
+
+urlpatterns = [
+ path('',index,name='index'),
+ path('login/',login,name='login'),
+ path('about/',about,name='about'),
+ path('contactus/',contact,name='contact'),
+ path('register/',register,name='register'),
+ path('detail/',detail,name='detail'),
+ path('searchtrain/',search,name="search"),
+ path('search/',fsearch,name="fsearch"),
+ path('pnr/', pnr, name='pnr'), 
+ path('canceltrain/',cancel,name="cancel"),
+ path('logout/',logout,name="logout"),
+ path('bookticket/',book,name="book"),
+ path('otp/<str:otp>/<str:username>/<str:password>/<str:email>/',otp, name='otp'),
+ path('cancel/',cancel_sucess,name='cancel_sucess')
+
+]
